3c257df190a30be942f9ea8ca6e812df903cd6f7
Vladimir Hazeleger <vladimir.hazeleger@gmail.com>
2024-12-11 23:06:54 +0100

    day 11 - finished old approach

--- Day 11/day11.py
+++ Day 11/day11.py
@@ -7,8 +7,7 @@ example = '125 17'
 
 input = [int(x) for x in input.split(' ')]
 
-from functools import cache, reduce
-from collections import Counter
+from functools import cache
 
 @cache
 def apply_rules(stone):
@@ -29,6 +28,37 @@ def blink(stone, n_times):
         # processed ORIGINAL stone value?
         # 
         return(sum([blink(s, n_times-1) for s in apply_rules(stone)]))
+
+def plutonian_stones(stones, n_times):
+    return(sum([blink(s, n_times) for s in stones]))
+
+part_1 = plutonian_stones(input, 25)
+part_2 = plutonian_stones(input, 75)
+
+### OLD CODE
+from functools import cache
+from collections import defaultdict
+
+@cache
+def apply_rules(stone):
+    if stone == 0:
+        return([1])
+    elif len(str(stone)) % 2 == 0:
+        return([int(str(stone)[0:int(len(str(stone))/2)]), int(str(stone)[int(len(str(stone))/2):])])
+    else:
+        return([stone*2024])
+    
+@cache
+def blink(stone, n_times):
+    stones = {stone: 1}
+    for i in range(0, n_times):
+        temp_dict = defaultdict(int)
+        for k, v in stones.items():
+            new_val = apply_rules(k)
+            for j in new_val: temp_dict[j] += v
+
+        stones = temp_dict
+    return(sum(stones.values()))
 
 def plutonian_stones(stones, n_times):
     return(sum([blink(s, n_times) for s in stones]))
